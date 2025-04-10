0cd2cb048420392dcd764d498c44691ad5542f82
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-10 17:49:07 -0400

    Update GlobalVideo.hx

--- source/GlobalVideo.hx
+++ source/GlobalVideo.hx
@@ -5,8 +5,6 @@ import openfl.Lib;
 class GlobalVideo
 {
 	private static var video:VideoHandler;
-	private static var webm:WebmHandler;
-	public static var isWebm:Bool = false;
 	public static var isAndroid:Bool = false;
 	public static var daAlpha1:Float = 0.2;
 	public static var daAlpha2:Float = 1;
@@ -20,26 +18,10 @@ class GlobalVideo
 	{
 		return video;
 	}
-	
-	public static function setWebm(vid:WebmHandler):Void
-	{
-		webm = vid;
-		isWebm = true;
-	}
-	
-	public static function getWebm():WebmHandler
-	{
-		return webm;
-	}
-	
+
 	public static function get():Dynamic
 	{
-		if (isWebm)
-		{
-			return getWebm();
-		} else {
-			return getVid();
-		}
+		return getVid();
 	}
 	
 	public static function calc(ind:Int):Dynamic
