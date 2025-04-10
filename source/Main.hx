05a6aebb1285c2a2aca8052176f4ba42ff1974be
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-10 17:51:56 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,103 +1,93 @@
-	package;
+package;
 
-	import flixel.FlxGame;
-	import flixel.FlxState;
-	import openfl.Assets;
-	import openfl.Lib;
-	import openfl.display.FPS;
-	import openfl.display.Sprite;
-	import openfl.events.Event;
+import flixel.FlxGame;
+import flixel.FlxState;
+import openfl.Assets;
+import openfl.Lib;
+import openfl.display.FPS;
+import openfl.display.Sprite;
+import openfl.events.Event;
 
-	class Main extends Sprite
-	{
-		var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-		var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-		var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
-		var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-		var framerate:Int = 144; // How many frames per second the game should run at.
-		var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-		var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-		public static var editor:Bool = false;
-		public static var woops:Bool = false;
-		public static var keyAmmo:Array<Int> = [4, 6, 9];
-		public static var dataJump:Array<Int> = [8, 12, 18];
-		public static var shaggyVoice:Bool = false;
-		public static var susVoice:Bool = false;
-	
-		// You can pretty much ignore everything from here on - your code should go in your states.
+class Main extends Sprite
+{
+var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+var framerate:Int = 144; // How many frames per second the game should run at.
+var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+public static var editor:Bool = false;
+public static var woops:Bool = false;
+public static var keyAmmo:Array<Int> = [4, 6, 9];
+public static var dataJump:Array<Int> = [8, 12, 18];
+public static var shaggyVoice:Bool = false;
+public static var susVoice:Bool = false;
 
-		public static function main():Void
-		{
-			Lib.current.addChild(new Main());
-		}
+// You can pretty much ignore everything from here on - your code should go in your states.
 
-		public function new()
-		{
-			super();
+public static function main():Void
+{
+Lib.current.addChild(new Main());
+}
 
-			if (stage != null)
-			{
-				init();
-			}
-			else
-			{
-				addEventListener(Event.ADDED_TO_STAGE, init);
-			}
-		}
+public function new()
+{
+super();
 
-		private function init(?E:Event):Void
-		{
-			if (hasEventListener(Event.ADDED_TO_STAGE))
-			{
-				removeEventListener(Event.ADDED_TO_STAGE, init);
-			}
+if (stage != null)
+{
+        init();
+}
+else
+{
+        addEventListener(Event.ADDED_TO_STAGE, init);
+}
+}
 
-			setupGame();
-		}
+private function init(?E:Event):Void
+{
+if (hasEventListener(Event.ADDED_TO_STAGE))
+{
+removeEventListener(Event.ADDED_TO_STAGE, init);
+}
 
-		private function setupGame():Void
-		{
-			var stageWidth:Int = Lib.current.stage.stageWidth;
-			var stageHeight:Int = Lib.current.stage.stageHeight;
+setupGame();
+}
 
-			if (zoom == -1)
-			{
-				var ratioX:Float = stageWidth / gameWidth;
-				var ratioY:Float = stageHeight / gameHeight;
-				zoom = Math.min(ratioX, ratioY);
-				gameWidth = Math.ceil(stageWidth / zoom);
-				gameHeight = Math.ceil(stageHeight / zoom);
-			}
+private function setupGame():Void
+{
+var stageWidth:Int = Lib.current.stage.stageWidth;
+var stageHeight:Int = Lib.current.stage.stageHeight;
 
-			#if !debug
-			initialState = TitleState;
-			#end
+if (zoom == -1)
+{
+var ratioX:Float = stageWidth / gameWidth;
+var ratioY:Float = stageHeight / gameHeight;
+zoom = Math.min(ratioX, ratioY);
+gameWidth = Math.ceil(stageWidth / zoom);
+gameHeight = Math.ceil(stageHeight / zoom);
+}
 
-			addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+#if !debug
+initialState = TitleState;
+#end
 
-			var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
+addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if web
-		var str1:String = "HTML CRAP";
-		var vHandler = new VideoHandler();
-		vHandler.init1();
-		vHandler.video.name = str1;
-		addChild(vHandler.video);
-		vHandler.init2();
-		GlobalVideo.setVid(vHandler);
-		vHandler.source(ourSource);
-		#elseif desktop
-		var str1:String = "WEBM SHIT"; 
-		var webmHandle = new WebmHandler();
-		webmHandle.source(ourSource);
-		webmHandle.makePlayer();
-		webmHandle.webm.name = str1;
-		addChild(webmHandle.webm);
-		GlobalVideo.setWebm(webmHandle);
-		#end
+var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
 
-			#if !mobile
-			addChild(new FPS(10, 3, 0xFFFFFF));
-			#end
-		}
-	}
+var str1:String = "HTML CRAP";
+var vHandler = new VideoHandler();
+vHandler.init1();
+vHandler.video.name = str1;
+addChild(vHandler.video);
+vHandler.init2();
+GlobalVideo.setVid(vHandler);
+vHandler.source(ourSource);
+
+#if !mobile
+addChild(new FPS(10, 3, 0xFFFFFF));
+#end
+}
+}
